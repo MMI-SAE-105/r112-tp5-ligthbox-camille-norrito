5d235a296205e05075871c1afe109bbacb0cd74d
camille-norrito <camille.norrito@edu.univ-fcomte.fr>
2024-12-03 15:21:23 +0100

    Ajout de l'ouverture Lightbox

--- src/js/script.js
+++ src/js/script.js
@@ -3,3 +3,10 @@ const thumbnails = document.querySelectorAll('ul li img');
 const lightbox = document.getElementById('lightbox');
 const lightboxImg = lightbox.querySelector('img');
 
+thumbnails.forEach((thumbnail) => {
+    thumbnail.addEventListener("click", (evt) => {
+        lightboxImg.src = thumbnail.dataset.fullImg;
+        lightbox.showModal();
+    });
+});
+
